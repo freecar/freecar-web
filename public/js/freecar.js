7fcc091372a18648a7b5f7e5b95e8c6afcb17c00
Grigor Yeghiazaryan <yeghiazaryan@gmail.com>
2013-07-23 21:48:04 +0400

    Remove facebook alerts

--- public/js/freecar.js
+++ public/js/freecar.js
@@ -136,13 +136,13 @@ $(function() {
           $(".modal:visible").modal("hide");
           delete self;
           if (!user.existed()) {
-            alert("User signed up and logged in through Facebook!");
+            // User signed up and logged in through Facebook
           } else {
-            alert("User logged in through Facebook!");
+            // User logged in through Facebook
           }
         },
         error: function(user, error) {
-          alert("User cancelled the Facebook login or did not fully authorize.");
+          // User cancelled the Facebook login or did not fully authorize
         }
       });
     },
