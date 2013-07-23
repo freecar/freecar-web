f70d138bab5b62a19ec8687a450d0125e217acb1
vgrigoryan <vahagn.grigoryan@learnship.de>
2013-07-24 00:23:34 +0400

    removed disabling of signup button on signup error

--- public/js/freecar.js
+++ public/js/freecar.js
@@ -1,299 +1,288 @@
 $(function() {
 
-  Parse.initialize("EIeotpfwEbYb60BJSXW6r8EPFF625L8vK9qgOA7R", "LKcBN7ZqSwwQX7OmY9Tv9xal2UhPh1RlpiaVGPzC");
+    Parse.initialize("EIeotpfwEbYb60BJSXW6r8EPFF625L8vK9qgOA7R", "LKcBN7ZqSwwQX7OmY9Tv9xal2UhPh1RlpiaVGPzC");
 
-  var Route = Parse.Object.extend("Routes", {
-    defaults: {
-      name: '',
-      from: '',
-      to: '',
-      time: '',
-      phone: '',
-      comment: ''
-    }
-  });
-
-  var Routes = Parse.Collection.extend({
-    model: Route
-  });
-
-  var RouteList = Parse.View.extend({
-    el: '.content',
-    render: function() {
-      var that = this;
-      var routes = new Routes;
-      var template = _.template($('#add-route-template').html());
-      that.$el.html(template);
-      routes.query = new Parse.Query(Route);
-      routes.query.equalTo('user', Parse.User.current());
-      routes.fetch({
-        success: function (routes) {
-          var template = _.template($('#route-list-template').html(), 
-            {routes: routes.models});
-          that.$el.html(template);
+    var Route = Parse.Object.extend("Routes", {
+        defaults: {
+            name: '',
+            from: '',
+            to: '',
+            time: '',
+            phone: '',
+            comment: ''
         }
-      })
-    },
-    events: {
-      'click .logout': 'logOut'
-    },
-    logOut: function(e) {
-      Parse.User.logOut();
-      new LogInView();
-      this.undelegateEvents();
-      delete this;
-    },
-  });
+    });
 
-  var EditRoute = Parse.View.extend({
-    el: '#editModal',
-    initialize: function() {
-      this.routes = new Routes;
-    },
-    render: function(options) {
-      if (options.id) {
-        var that = this;
-        that.route = new Route({id: options.id});
-        that.route.fetch({
-          success: function(route) {
-            var template = _.template($('#edit-route-template').html(), {route: route});
+    var Routes = Parse.Collection.extend({
+        model: Route
+    });
+
+    var RouteList = Parse.View.extend({
+        el: '.content',
+        render: function() {
+            var that = this;
+            var routes = new Routes;
+            var template = _.template($('#add-route-template').html());
             that.$el.html(template);
-            that.$el.modal("show");
+            routes.query = new Parse.Query(Route);
+            routes.query.equalTo('user', Parse.User.current());
+            routes.fetch({
+                success: function(routes) {
+                    var template = _.template($('#route-list-template').html(),
+                            {routes: routes.models});
+                    that.$el.html(template);
+                }
+            })
+        },
+        events: {
+            'click .logout': 'logOut'
+        },
+        logOut: function(e) {
+            Parse.User.logOut();
+            new LogInView();
+            this.undelegateEvents();
+            delete this;
+        },
+    });
+
+    var EditRoute = Parse.View.extend({
+        el: '#editModal',
+        initialize: function() {
+            this.routes = new Routes;
+        },
+        render: function(options) {
+            if (options.id) {
+                var that = this;
+                that.route = new Route({id: options.id});
+                that.route.fetch({
+                    success: function(route) {
+                        var template = _.template($('#edit-route-template').html(), {route: route});
+                        that.$el.html(template);
+                        that.$el.modal("show");
+                        router.navigate('', {trigger: true});
+                    }
+                })
+            } else {
+                var template = _.template($('#edit-route-template').html(), {route: null});
+                this.$el.html(template);
+                this.$el.modal("show");
+                router.navigate('', {trigger: true});
+            }
+        },
+        events: {
+            'submit .edit-route-form': 'saveRoute',
+            'click .delete': 'deleteRoute',
+            'click .cancel': 'cancel'
+        },
+        saveRoute: function(ev) {
+            var that = this;
+            var route = new Route;
+            route.set('name', this.$('.name').val());
+            route.set('from', this.$('.from ').val());
+            route.set('to', this.$('.to').val());
+            route.set('time', this.$('.time').val());
+            route.set('phone', this.$('.phone').val());
+            route.set('comment', this.$('.comment').val());
+            route.set('user', Parse.User.current());
+            route.set('id', this.$('.id').val());
+            route.save(null, {
+                success: function(route) {
+                    router.navigate('', {trigger: true});
+                    routeList.render();
+                },
+                error: function(route, error) {
+                    console.log('error: ' + error.code + ' ' + error.message);
+                }
+            });
+            $(".modal:visible").modal("hide");
+            return false;
+        },
+        deleteRoute: function(ev) {
+            this.route.destroy({
+                success: function() {
+                    router.navigate('', {trigger: true});
+                    routeList.render();
+                    $(".modal:visible").modal("hide");
+                }
+            })
+            return false;
+        },
+        cancel: function(ev) {
             router.navigate('', {trigger: true});
-          }
-        })
-      } else {
-        var template = _.template($('#edit-route-template').html(), {route: null});
-        this.$el.html(template);
-        this.$el.modal("show");
-        router.navigate('', {trigger: true});
-      }
-    },
-    events: {
-      'submit .edit-route-form': 'saveRoute',
-      'click .delete': 'deleteRoute',
-      'click .cancel': 'cancel'
-    },    
-    saveRoute: function(ev) {
-      var that = this;
-      var route = new Route;
-      route.set('name',    this.$('.name').val());
-      route.set('from',    this.$('.from ').val());
-      route.set('to',      this.$('.to').val());
-      route.set('time',    this.$('.time').val());
-      route.set('phone',   this.$('.phone').val());
-      route.set('comment', this.$('.comment').val());
-      route.set('user', Parse.User.current());
-      route.set('id', this.$('.id').val());
-      route.save(null, {
-        success: function(route) {
-          router.navigate('', {trigger: true});
-          routeList.render();
+        }
+    });
+
+    var LogInView = Parse.View.extend({
+        events: {
+            "submit form.login-form": "logIn",
+            "submit form.signup-form": "signUp",
+            "click .fb-login": "facebookLogin"
         },
-
-        error: function(route, error) {
-          console.log('error: ' + error.code + ' ' + error.message);
-        }
-      });
-      $(".modal:visible").modal("hide");
-      return false;
-    },
-    deleteRoute: function(ev) {
-      this.route.destroy({
-        success: function() {
-          router.navigate('', {trigger: true});
-          routeList.render();
-          $(".modal:visible").modal("hide");
-        }
-      })
-      return false;
-    },
-    cancel: function(ev) {
-      router.navigate('', {trigger: true});
-    }
-  });
-
-  var LogInView = Parse.View.extend({
-    events: {
-      "submit form.login-form": "logIn",
-      "submit form.signup-form": "signUp",
-      "click .fb-login": "facebookLogin"
-    },
-
-    el: ".content",
-
-    initialize: function() {
-      _.bindAll(this, "logIn", "signUp", "facebookLogin");
-      this.render();
-    },
-
-    facebookLogin: function(e) {
-      var self = this;
-      Parse.FacebookUtils.logIn('email', {
-        success: function(user) {
-          routeList.render();
-          self.undelegateEvents();
-          
-          delete self;
-          if (!user.existed()) {
-            // User signed up and logged in through Facebook
-          } else {
-            // User logged in through Facebook
-          }
+        el: ".content",
+        initialize: function() {
+            _.bindAll(this, "logIn", "signUp", "facebookLogin");
+            this.render();
         },
-        error: function(user, error) {
-          // User cancelled the Facebook login or did not fully authorize
-        }
-      });
-      $(".modal:visible").modal("hide");
-    },
+        facebookLogin: function(e) {
+            var self = this;
+            Parse.FacebookUtils.logIn('email', {
+                success: function(user) {
+                    routeList.render();
+                    self.undelegateEvents();
 
-    logIn: function(e) {
-      var self = this;
-      var username = this.$("#login-username").val();
-      var password = this.$("#login-password").val();
-
-      Parse.User.logIn(username, password, {
-        success: function(user) {
-          routeList.render();
-          self.undelegateEvents();
-          delete self;
+                    delete self;
+                    if (!user.existed()) {
+                        // User signed up and logged in through Facebook
+                    } else {
+                        // User logged in through Facebook
+                    }
+                },
+                error: function(user, error) {
+                    // User cancelled the Facebook login or did not fully authorize
+                }
+            });
+            $(".modal:visible").modal("hide");
         },
+        logIn: function(e) {
+            var self = this;
+            var username = this.$("#login-username").val();
+            var password = this.$("#login-password").val();
 
-        error: function(user, error) {
-          self.$(".login-form .error").html("<button type=\"button\" class=\"close\" data-dismiss=\"alert\">&times;</button>Invalid username or password. Please try again.").show();
-          this.$(".login-form button").removeAttr("disabled");
-        }
-      });
+            Parse.User.logIn(username, password, {
+                success: function(user) {
+                    routeList.render();
+                    self.undelegateEvents();
+                    delete self;
+                },
+                error: function(user, error) {
+                    self.$(".login-form .error").html("<button type=\"button\" class=\"close\" data-dismiss=\"alert\">&times;</button>Invalid username or password. Please try again.").show();
+                    this.$(".login-form button").removeAttr("disabled");
+                }
+            });
 
-      $(".modal:visible").modal("hide");
-      this.$(".login-form button").attr("disabled", "disabled");
+            $(".modal:visible").modal("hide");
+            this.$(".login-form button").attr("disabled", "disabled");
 
-      return false;
-    },
-
-    signUp: function(e) {
-      var self = this;
-      var username = this.$("#signup-username").val();
-      var password = this.$("#signup-password").val();
-
-      Parse.User.signUp(username, password, { ACL: new Parse.ACL() }, {
-        success: function(user) {
-          routeList.render()
-          self.undelegateEvents();
-          delete self;
+            return false;
         },
+        signUp: function(e) {
+            var self = this;
+            var username = this.$("#signup-username").val();
+            var password = this.$("#signup-password").val();
 
-        error: function(user, error) {
-          self.$(".signup-form .error").html(error.message).show();
-          this.$(".signup-form button").removeAttr("disabled");
+            Parse.User.signUp(username, password, {ACL: new Parse.ACL()}, {
+                success: function(user) {
+                    routeList.render()
+                    self.undelegateEvents();
+                    delete self;
+                },
+                error: function(user, error) {
+                    self.$(".signup-form .error").html(error.message).show();
+                    this.$(".signup-form button").removeAttr("disabled");
+                }
+            });
+
+            return false;
+        },
+        render: function() {
+            var that = this;
+            var routes = new Routes;
+            routes.query = new Parse.Query(Route);
+            routes.fetch({
+                success: function(routes) {
+                    var template = _.template($('#public-view-template').html(),
+                            {routes: routes.models});
+                    that.$el.html(template);
+
+                    //updateTables();
+                    //init dataTable
+
+
+                    var oTable = $('#routestable').dataTable({
+                        "bPaginate": false,
+                        "bLengthChange": false,
+                        "oLanguage": {"sSearch": "Փնտրել"}
+                    });
+
+
+                    $("body").delegate("a.use-this-filter", "click", function(ev) {
+                        ev.preventDefault();
+                        oTable.fnFilter($(this).text());
+                        $(".back-to-routes span").html($(this).text());
+                        $(".back-to-routes-container").show();
+                        $(".dataTables_filter").hide();
+                    });
+
+                    // $("a.use-this-filter").click(function(ev){
+
+                    // });
+                    $(".back-to-routes").click(function() {
+                        oTable.fnFilter("");
+                        $(".back-to-routes-container").hide();
+                        $(".dataTables_filter").show();
+                    });
+                    //  setTimeout(function(){
+                    //    updateTables();
+                    // },300);
+                }
+            })
+            this.delegateEvents();
+
         }
-      });
+    });
 
-      this.$(".signup-form button").attr("disabled", "disabled");
-
-      return false;
-    },
-
-    render: function() {
-      var that = this;
-      var routes = new Routes;
-      routes.query = new Parse.Query(Route);
-      routes.fetch({
-        success: function (routes) {
-          var template = _.template($('#public-view-template').html(), 
-            {routes: routes.models});
-          that.$el.html(template);
-          
-          //updateTables();
-          //init dataTable
-
-         
-          var oTable = $('#routestable').dataTable({
-          "bPaginate": false,
-          "bLengthChange": false,
-          "oLanguage": { "sSearch": "Փնտրել" } 
-          });
-
-
-          $("body").delegate("a.use-this-filter", "click", function(ev) {
-            ev.preventDefault();
-            oTable.fnFilter( $(this).text() );
-            $(".back-to-routes span").html($(this).text());
-            $(".back-to-routes-container").show();
-            $(".dataTables_filter").hide();
-          });
-
-          // $("a.use-this-filter").click(function(ev){
-            
-          // });
-          $(".back-to-routes").click(function(){
-            oTable.fnFilter("");
-            $(".back-to-routes-container").hide();
-            $(".dataTables_filter").show();
-          });
-          //  setTimeout(function(){
-          //    updateTables();
-          // },300);
+    var ReviewView = Parse.View.extend({
+        el: '.content',
+        initialize: function(id) {
+            this.render(id);
+        },
+        // @todo: Ideally the permalink method should resist in Route class
+        permalink: function(id) {
+            return window.location.hostname + '/route/' + id;
+        },
+        render: function(id) {
+            var that = this;
+            var template = _.template($('#reviews-route-template').html());
+            that.$el.html(template);
+            that.$('div.fb-comments').data('href', this.permalink(id));
+            // FB SDK is doing one-time parse at the end of page load
+            // We will need to do re-parse on other cases.
+            if (!_.isUndefined(FB)) {
+                FB.XFBML.parse();
+            }
         }
-      })  
-      this.delegateEvents();
+    })
 
-    }
-  });
+    var routeList = new RouteList;
+    var editRoute = new EditRoute;
 
-  var ReviewView = Parse.View.extend({
-    el: '.content',
-    initialize: function(id) {
-      this.render(id);
-    },
-    // @todo: Ideally the permalink method should resist in Route class
-    permalink: function(id) {
-      return window.location.hostname + '/route/' + id;
-    },
-    render: function(id) {
-      var that = this;
-      var template = _.template($('#reviews-route-template').html());
-      that.$el.html(template);
-      that.$('div.fb-comments').data('href', this.permalink(id));
-      // FB SDK is doing one-time parse at the end of page load
-      // We will need to do re-parse on other cases.
-      if(!_.isUndefined(FB)) {
-        FB.XFBML.parse();
-      }
-    }
-  })
+    var Router = Parse.Router.extend({
+        routes: {
+            '': 'home',
+            'new': 'new',
+            'edit/:id': 'edit',
+            'reviews/:id': 'reviews'
+        },
+        home: function() {
+            if (Parse.User.current()) {
+                routeList.render();
+            } else {
+                new LogInView();
+            }
+        },
+        new : function() {
+            editRoute.render({});
+        },
+        edit: function(id) {
+            editRoute.render({id: id});
+        },
+        reviews: function(id) {
+            new ReviewView(id);
+        }
+    });
 
-  var routeList = new RouteList;
-  var editRoute = new EditRoute;
+    var router = new Router;
 
-  var Router = Parse.Router.extend({
-    routes: {
-      '': 'home',
-      'new': 'new',
-      'edit/:id': 'edit',
-      'reviews/:id': 'reviews'
-    },
-    home: function() {
-      if (Parse.User.current()) {
-        routeList.render();
-      } else {
-        new LogInView();
-      }
-    },
-    new: function() {
-      editRoute.render({});
-    },
-    edit: function(id) {
-      editRoute.render({id: id});
-    },
-    reviews: function(id) {
-      new ReviewView(id);
-    }
-  });
-
-  var router = new Router;
-
-  Parse.history.start();
+    Parse.history.start();
 
 });
